aabb6e220f1e44229e6ae54da22b9639b1948b1b
Edem Akato <augustine.akato@amalitech.com>
2024-11-05 16:00:38 +0000

    comments and structuring

--- src/Statistics.java
+++ src/Statistics.java
@@ -6,26 +6,27 @@ public class Statistics {
         int n = scores.length;
         double average , sum =0;
 
-        Arrays.sort(scores);
 
-        int max = scores[scores.length -1];
+        Arrays.sort(scores);                                    //sort the array in ascending order
 
-        int min = scores[0];
+        int max = scores[scores.length -1];                      //maximum score is the last element of the array
 
-        for(int i : scores)
+        int min = scores[0];                                     //minimum score is the first element of the array
+
+        for(int i : scores)                                      //calculate the sum of all the scores and find the average
             sum += i;
 
         average = sum / n;
-
+                                                                //print the results of maximum , minimum and the array
         System.out.println(
                 "Values: \n" + "\n" +
                         "The maximum grade is " + max + "\n" +
                         "The minimum grade is " + min + "\n" +
                         "The average grade is " + average
         );
-
+                                                                //initialize new array for the stats
         int [] stats  = new int[5];
-
+                                                                //assign the frequencies of each range to an element of the array
         for(int score : scores){
             stats[0] += (score >=0 && score <=20)? 1 : 0;
             stats[1] += (score >20 && score <41)? 1 : 0;
@@ -34,13 +35,13 @@ public class Statistics {
             stats[4] += (score > 80) ? 1 : 0;
         }
 
-
+                                                                //find the highest frequency. use it to create the stack of bars
         int maxHeight = 0;
         for (int stat : stats){
             if(stat > maxHeight)
                 maxHeight = stat;
         }
-
+                                                                //for every level, print the bar or a space for the particular range
         for(int i = maxHeight ; i > 0; i--){
             for (int stat: stats){
                 if(stat >= i) {
